ec136b1a4bfc55fb04d5a6a866011e82fa4a0db8
Mohammed-Arafat <116948852+Mohammed-Arafat@users.noreply.github.com>
2022-10-30 19:23:50 +0600

    Sheet 4 --> C -- added again

--- Newcomer Sheets/sheet 4/C__Compare.c
+++ Newcomer Sheets/sheet 4/C__Compare.c
@@ -7,7 +7,29 @@ int main ()
 {
     char x[30], y[30];
 
+    int v;
+
     scanf ("%s%s", x, y);
+
+    v = strcmp (x , y);
+
+    if (v < 0)
+
+    {
+        printf ("%s\n", x);
+    }
+
+    else if (v > 0)
+    {
+        printf ("%s\n", y);
+    }
+
+    else
+    {
+        printf ("%s\n", x);
+    }
+    
+    
 
     return 0;
 }
